4f50eadde71826f5b8f61781e83c71b064f00cd0
Konain Hassan <konainhassan604@gmail.com>
2026-01-30 00:46:00 +0500

    update

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex flex-col items-center justify-center container'>
+      
+    </div>
+  )
+}
+
+export default About
